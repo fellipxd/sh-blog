b2a80a4152cb9cc9866c8db7f96af8d98f4b5112
Collins Adaigho <donsmatt44@gmail.com>
2023-05-06 12:10:50 +0100

    Edit and Delete done

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,25 +1,32 @@
 import { CiEdit } from "react-icons/ci";
 import { RiDeleteBin3Line } from "react-icons/ri";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 // import Menu from "../components/Menu";
 import { useEffect, useState } from "react";
+import { getText } from "../utils/getText";
 import Picture from "../img/marguerite-729510__340.jpg";
 
 const Single = () => {
   const [posts, setPosts] = useState("");
   // const [fetching, isFetching] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errMessage, setErrMessage] = useState("");
+
+  const navigate = useNavigate();
 
   const { id } = useParams();
 
   console.log(id);
 
-  // const user = sessionStorage.getItem("id");
+  const user = sessionStorage.getItem("id");
 
-  // const userId = parseInt(user);
+  const user_id = parseInt(user);
 
   useEffect(() => {
     async function blogs() {
-      const res = await fetch(`https://blog.shbootcamp.com.ng/fetch_post.php?id=${id}`);
+      const res = await fetch(
+        `https://blog.shbootcamp.com.ng/fetch_post.php?id=${id}`
+      );
       const data = await res.json();
       const blog = data.fetch_post.post;
       console.log(data, blog);
@@ -27,6 +34,38 @@ const Single = () => {
     }
     blogs();
   }, [setPosts, id]);
+
+  const deletePost = () => {
+    console.log("deleted");
+
+    const deleteData = { id, user_id };
+
+    fetch("https://blog.shbootcamp.com.ng/delete_post.php", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(deleteData),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.status === "success") {
+          setSuccessMessage(data.message);
+          setTimeout(() => {
+            setSuccessMessage("");
+            navigate("/home");
+          }, 3000);
+        } else {
+          setErrMessage(data.message);
+          setTimeout(() => {
+            setErrMessage("");
+          }, 3000);
+        }
+      });
+  };
 
   return (
     <div className="px-6 flex gap-[50px]">
@@ -51,20 +90,24 @@ const Single = () => {
                     <p>{post.date_publish}</p>
                   </div>
                   <span className="flex gap-2 -mt-4">
-                    <Link to={`/write?edit=2`} state={post}>
+                    <Link to={`/write?edit=${id}`} state={post}>
                       {
                         <CiEdit className="text-2xl cursor-pointer hover:text-blue-600" />
                       }
                     </Link>
-                    {
-                      <RiDeleteBin3Line className=" text-2xl cursor-pointer hover:text-red-500" />
-                    }
+                    <span onClick={deletePost}>
+                      {
+                        <RiDeleteBin3Line className=" text-2xl cursor-pointer hover:text-red-500" />
+                      }
+                    </span>
                   </span>
                 </div>
               </div>
+              <span className="text-green-400">{successMessage}</span>
+              <span className="text-red-500">{errMessage}</span>
               <h1 className="text-4xl font-bold">{post.blog_title}</h1>
               <div className="text-lg text-justify flex flex-col gap-2 ">
-                {post.body}
+                {getText(post.body)}
               </div>
             </div>
           ))}
